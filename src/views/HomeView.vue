8d282fec8430d5213ffe5918141f4cc03b07e7d5
aim <ahmadhakim404@gmail.com>
2024-01-25 23:25:25 +0700

    membuat animasi transisi masuk dan transisi keluar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,8 +20,17 @@ const showText = ref(false);
 .fade-enter-to {
 	opacity: 1;
 }
-
 .fade-enter-active {
+	transition: all 1s ease;
+}
+
+.fade-leave-from {
+	opacity: 1;
+}
+.fade-leave-to {
+	opacity: 0;
+}
+.fade-leave-active {
 	transition: all 1s ease;
 }
 </style>
