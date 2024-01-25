deafb768e26e1a322ef228e1b7dcccd84bb5f1ac
aim <ahmadhakim404@gmail.com>
2024-01-26 00:18:16 +0700

    membuat animasi transisi masuk dengan TransitionGroup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,14 +20,16 @@ function removeTask(task) {
 	<main>
 		<div class="container">
 			<input type="text" v-model="newTask" @keyup.enter="addTask()" autofocus />
-			<div
-				class="card-list"
-				v-for="task in tasks"
-				:key="task"
-				@click="removeTask(task)"
-			>
-				{{ task }}
-			</div>
+			<TransitionGroup name="list">
+				<div
+					class="card-list"
+					v-for="task in tasks"
+					:key="task"
+					@click="removeTask(task)"
+				>
+					{{ task }}
+				</div>
+			</TransitionGroup>
 		</div>
 	</main>
 </template>
@@ -55,5 +57,17 @@ function removeTask(task) {
 	box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
 	text-align: center;
 	cursor: pointer;
+}
+
+.list-enter-from {
+	opacity: 0;
+	transform: scale(0.6);
+}
+.list-enter-to {
+	opacity: 1;
+	transform: scale(1);
+}
+.list-enter-active {
+	transition: all 0.5s ease;
 }
 </style>
